6f978664c5e42447abc24f3ccc68b14852c8f752
Rajid Ansari <rajidansari25@gmail.com>
2025-12-25 13:42:28 +0530

    configure cors

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(cors({
     // origin: "https://passguard0.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://passguard0.vercel.app",
     credentials: true,
 }))
 
